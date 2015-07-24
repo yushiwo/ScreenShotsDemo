960e0877c4ae51388285c94c4d82b23daba66c97
zr <zhengrui504@163.com>
2015-07-24 15:32:10 +0800

    增加了本地视频播放，实时截图实现

--- app/src/main/java/com/netease/screenshotsdemo/MainActivity.java
+++ app/src/main/java/com/netease/screenshotsdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.netease.screenshotsdemo;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -25,18 +26,23 @@ public class MainActivity extends ActionBarActivity {
      * 某个view的截图
      */
     private Button mViewShotBtn;
+    private Button mLocalVideoShotBtn;
 
     private ImageView mImg;
     private VideoView mVideoView;
+
+    private Uri mVideoUri;
+    private String mVideoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initView();
+        initData();
         setListeners();
 
-        playVideo();
+        playLocalVideo();
     }
 
     /**
@@ -45,8 +51,14 @@ public class MainActivity extends ActionBarActivity {
     private void initView(){
         mBtn = (Button)findViewById(R.id.btn);
         mViewShotBtn = (Button)findViewById(R.id.btn_shot_view);
+        mLocalVideoShotBtn = (Button)findViewById(R.id.btn_shot_video_local);
         mImg = (ImageView)findViewById(R.id.img);
         mVideoView = (VideoView)findViewById(R.id.videoview);
+    }
+
+    private void initData(){
+        mVideoUri = Uri.parse("http://v.cctv.com/flash/mp4video28/TMS/2013/05/06/265114d5f2e641278098503f1676d017_h264418000nero_aac32-1.mp4");
+        mVideoPath = "/sdcard/test.mp4";
     }
 
     /**
@@ -66,12 +78,31 @@ public class MainActivity extends ActionBarActivity {
                 getBitmapByView(mImg);
             }
         });
+
+        mLocalVideoShotBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shotLocalVideo();
+            }
+        });
     }
 
-    private void playVideo(){
-        Uri uri = Uri.parse("http://v.cctv.com/flash/mp4video28/TMS/2013/05/06/265114d5f2e641278098503f1676d017_h264418000nero_aac32-1.mp4");
+    /**
+     * 播放网络的视频
+     */
+    private void playRemoteVideo(){
         mVideoView.setMediaController(new MediaController(this));
-        mVideoView.setVideoURI(uri);
+        mVideoView.setVideoURI(mVideoUri);
+        mVideoView.requestFocus();
+        mVideoView.start();
+    }
+
+    /**
+     * 播放本地视频
+     */
+    private void playLocalVideo(){
+        mVideoView.setMediaController(new MediaController(this));
+        mVideoView.setVideoPath(mVideoPath);
         mVideoView.requestFocus();
         mVideoView.start();
     }
@@ -103,6 +134,18 @@ public class MainActivity extends ActionBarActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return bitmap;
+    }
+
+    /**
+     * 本地视频播放截屏
+     * @return
+     */
+    private Bitmap shotLocalVideo(){
+        MediaMetadataRetriever rev = new MediaMetadataRetriever();
+        rev.setDataSource(mVideoPath);
+        Bitmap bitmap = rev.getFrameAtTime(mVideoView.getCurrentPosition() * 1000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
+        mImg.setImageBitmap(bitmap);
         return bitmap;
     }
 
